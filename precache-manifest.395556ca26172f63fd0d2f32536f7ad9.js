ba97f8528b34acbdc6395ba1ee98f1dc084c9074
Tran Cao Trung <caotrung4691@gmail.com>
2020-03-25 21:40:46 +0700

    Updates

---
+++ precache-manifest.395556ca26172f63fd0d2f32536f7ad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e84e942351fbe2be4a32a493b01fa53",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "7d18e662de08fa65fb9f",
+    "url": "/ReactJS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/ReactJS/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d18e662de08fa65fb9f",
+    "url": "/ReactJS/static/js/main.e80585b1.chunk.js"
+  },
+  {
+    "revision": "2e86f84af9031fe2c8c7",
+    "url": "/ReactJS/static/js/runtime-main.37abde28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TCTR7/ReactJS.git/precache-manifest.68a96096b9f1ec69412ded2a32276148.js"
+  "/ReactJS/precache-manifest.395556ca26172f63fd0d2f32536f7ad9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TCTR7/ReactJS.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactJS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
